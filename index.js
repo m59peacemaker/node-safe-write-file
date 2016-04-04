98a7f3a1b7ab9e5b5c9edc7a19a4afa33901736f
Johnny Hauser <johnnyhauser@gmail.com>
2016-04-04 13:01:17 -0500

    1.1.0 don't swallow mkdir errors

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
-var fs       = require('fs');
-var path     = require('path');
-var mkdirp   = require('mkdirp');
+var fs       = require('fs')
+var path     = require('path')
+var mkdirp   = require('mkdirp')
 
-module.exports = function(filepath) {
-  var args = arguments;
-  mkdirp(path.dirname(filepath), function() {
-    fs.writeFile.apply(fs, args);
-  });
-};
+module.exports = function(filepath, data, options, cb) {
+  var fsArgs = [].slice.call(arguments)
+  if (typeof options === 'function' && typeof cb === 'undefined') {
+    cb = options
+    options = null
+  }
+  var mkdirpArgs = [path.dirname(filepath)]
+  if (options) {
+    mkdirpArgs.push(options)
+  }
+  mkdirpArgs.push(function(err) {
+    if (err) { return cb(err) }
+    fs.writeFile.apply(fs, fsArgs)
+  })
+  mkdirp.apply(null, mkdirpArgs)
+}
--- test/index.js
+++ test/index.js
@@ -6,9 +6,10 @@ var safeWriteFile = require('../');
 var tmp = __dirname+'/tmp';
 
 test('creates directories for file and writes it', function(t) {
-  t.plan(1);
+  t.plan(2);
   var dest = tmp+'/foo/bar/baz.txt';
-  safeWriteFile(dest, 'Text, bro!', function() {
+  safeWriteFile(dest, 'Text, bro!', function(err) {
+    t.false(err)
     fs.stat(dest, function(err, stats) {
       rimraf(tmp, function() {
         t.false(err);
